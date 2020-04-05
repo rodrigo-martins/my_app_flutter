968ec63e6424a6da3bb3d3776b0e8877ee9ea33b
Rodrigo Siqueira <rodrigo.siqueira@visie.com.br>
2020-04-05 17:16:29 -0300

    class 105

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -25,7 +25,7 @@ class Chart extends StatelessWidget {
       }
 
       return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
-    });
+    }).reversed.toList();
   }
 
   double get maxSpending {
