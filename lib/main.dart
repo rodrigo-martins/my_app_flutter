17f44aa8163b68d86d07d497e7da2c5aa1f81cc2
Rodrigo Siqueira <rodrigo.siqueira@visie.com.br>
2020-04-12 12:25:50 -0300

    Class 126

--- lib/main.dart
+++ lib/main.dart
@@ -97,8 +97,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Personal expenses'),
       actions: <Widget>[
@@ -109,14 +109,14 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final txChartWidget = Container(
-      height: (MediaQuery.of(context).size.height -
+      height: (mediaQuery.size.height -
               appBar.preferredSize.height -
-              MediaQuery.of(context).padding.top) *
+              mediaQuery.padding.top) *
           (isLandscape ? .6 : .3),
       child: Chart(_recentTransactions),
     );
     final txListWidget = Container(
-        height: MediaQuery.of(context).size.height * .7,
+        height: mediaQuery.size.height * .7,
         child: TransactionList(_userTransaction, _deleteTransaction));
     return Scaffold(
       appBar: appBar,
